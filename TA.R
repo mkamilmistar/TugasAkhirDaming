2bcacdc76e1b62db17a09c08ad8776fa779bbcda
mkamilmistar <kamil_mistar@apps.ipb.ac.id>
2020-04-28 16:51:44 +0700

    tot

--- TA.R
+++ TA.R
@@ -1,5 +1,8 @@
 data <- read.csv("~/R/TA/bank.csv", header = TRUE, sep = ";")
-library(mice)
+str(data)
+summary(data)
+
+#library(mice)
 md.pattern(data)
 
 boxplot(data$age)
@@ -8,6 +11,15 @@ plot(density(data$age), main="Density Plot For Age")
 boxplot(data$duration)
 plot(density(data$duration), main="Density Plot For Duration")
 
-dataBank = data
 
-#tes
+set.seed(1234)
+ind <- sample(2, nrow(data), replace=TRUE, prob=c(0.7, 0.3))
+trainData <- data[ind==1,]
+testData <- data[ind==2,]
+
+myFormula <- y ~ age + job + marital + education + default + balance + housing + loan + contact + day + month + duration  + campaign + pdays + previous + poutcome
+iris_ctree <- ctree(myFormula, data=trainData)
+
+print(iris_ctree)
+plot(iris_ctree)
+plot(iris_ctree, type="simple")
