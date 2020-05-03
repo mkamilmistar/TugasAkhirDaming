7a7727ede7c1a3276646fa919be50f8f88e0a7f3
mkamilmistar <kamil_mistar@apps.ipb.ac.id>
2020-05-03 23:00:27 +0700

    mantap

--- TA.R
+++ TA.R
@@ -1,13 +1,78 @@
 data <- read.csv("~/R/TA/bank.csv", header = TRUE, sep = ";")
 str(data)
-summary(data$balance)
+summary(data)
 
 
 data$balance[data$balance <= 0] <- NA
 data$balance[is.na(data$balance)] <- mean (data$balance, na.rm= TRUE)
+summary(data$balance)
+
+job <- data$job
+job <- as.data.frame(job)
+deposit <- data$y
+deposit <- as.data.frame(deposit)
+
+bank_data <- cbind(job, deposit)
+
+summary(bank_data)
+
+#who deposit vs job category
+job_data <- bank_data[which(bank_data$deposit=="yes"), ]
+summary(job_data)
+
+#reduksi data job
+data_temp <- NULL
+data_temp[data$job=="retired" | data$job=="student" | data$job == "unemployed" | data$job == "unknown"] <- "other"
+data_temp[data$job=="services" | data$job=="housemaid"] <- "pink-collar"
+data_temp[data$job=="management" | data$job=="admin."] <- "white-collar"
+data_temp[data$job=="blue-collar"] <- "blue-collar"
+data_temp[data$job=="technician"] <- "technician"
+data_temp[data$job=="self-employed"] <- "self-employed"
+data_temp[data$job=="entrepreneur"] <- "entrepreneur"
+data$job <- data_temp
+
+summary(data)
 
 library(mice)
 md.pattern(data)
+
+#reduksi data poutcome
+data_temp[data$poutcome=="unknown" | data$poutcome=="other"] <- "unknown"
+data_temp[data$poutcome=="failure"] <- "failure"
+data_temp[data$poutcome=="success"] <- "success"
+data$poutcome <- data_temp
+summary(data$poutcome)
+
+#delete contact, karna gabutuh
+data$contact<-NULL
+
+#ubah nilai default, housing and loan
+data_temp <- NULL
+data_temp[data$default=="yes"] <- 1
+data_temp[data$default=="no"] <- 0
+data_temp[data$housing=="yes"] <- 1
+data_temp[data$housing=="no"] <- 0
+data_temp[data$loan=="yes"] <- 1
+data_temp[data$loan=="no"] <- 0
+data$default<-data_temp
+data$housing<-data_temp
+data$loan<-data_temp
+summary(data)
+
+#hapus bulan dan hari
+data$month<-NULL
+data$day<-NULL
+
+#ubah nilai deposit
+data_temp <- NULL
+data_temp[data$y=="yes"] <- 1
+data_temp[data$y=="no"] <- 0
+data$y<-data_temp
+
+#ubah nilai pdays -1 ke 1000
+data$pdays[data$pdays==-1] <- 1000
+
+#buat colom baru recent_days dan drop pdays
 
 boxplot(data$age)
 plot(density(data$age), main="Density Plot For Age")
@@ -16,14 +81,4 @@ boxplot(data$duration)
 plot(density(data$duration), main="Density Plot For Duration")
 
 
-set.seed(1234)
-ind <- sample(2, nrow(data), replace=TRUE, prob=c(0.7, 0.3))
-trainData <- data[ind==1,]
-testData <- data[ind==2,]
 
-myFormula <- y ~ age + job + marital + education + default + balance + housing + loan + contact + day + month + duration  + campaign + pdays + previous + poutcome
-iris_ctree <- ctree(myFormula, data=trainData)
-
-print(iris_ctree)
-plot(iris_ctree)
-plot(iris_ctree, type="simple")
